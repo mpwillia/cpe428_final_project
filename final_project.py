37c722d157e2db8a0618938259bccb913a5b148a
Michael Williams <mike@clwill.com>
2017-03-08 11:21:30 -0800

    added letter segmentation

--- final_project.py
+++ final_project.py
@@ -1,11 +1,29 @@
 
 import dataset
 
+from skimage import data
+from skimage import transform as tf
+
+import ocr_util
 
 def main():
     print("CPE 428 Final Project")
 
-    dataset.chars74k.load_english()
+    #dataset.chars74k.load_english()
+
+    #test_img_path = "ocr_test_img1.png"
+    test_img_path = "ocr_test_red_boat.png"
+    test_img = load_image(test_img_path, as_grey = True, final_size = None)
+    
+    letter_imgs = ocr_util.segment_letters(test_img)
+    print("Found {:d} Letters".format(len(letter_imgs)))
+
+def load_image(image_path, as_grey = False, final_size = (64, 64)):
+    img = data.imread(image_path, as_grey = as_grey)
+    if final_size is not None:
+        return tf.resize(img, final_size)
+    else:
+        return img
 
 if __name__ == "__main__":
     main()
---
+++ ocr_util.py
@@ -0,0 +1,124 @@
+
+import numpy as np
+
+from skimage.external.tifffile import imshow
+from matplotlib import pyplot
+
+
+def segment_letters(image):
+    
+    norm_image = normalize_image_values(image)
+    thresh_image = norm_image < 0.5
+    
+    crop_bounds = get_crop_bounds(thresh_image, padding = 10)
+    cropped_img = crop_image(norm_image, *crop_bounds)
+    
+    # now that we're just looking at the image content, lets segment
+    thresh_image = cropped_img < 0.5
+    col_chunks = find_col_chunks(thresh_image)
+
+    img_chunks = crop_col_chunks(cropped_img, col_chunks)
+    img_chunks = [make_image_square(img) for img in img_chunks]
+
+    for img_chunk in img_chunks:
+        imshow(img_chunk)
+    
+    return img_chunks
+
+
+def make_image_square(image):
+    img_h, img_w = image.shape
+    dom_size = max(image.shape) 
+    
+    pad_w = dom_size - img_w
+    pad_wl = int(pad_w / 2.0)
+    pad_wr = pad_w - pad_wl
+
+    pad_h = dom_size - img_h
+    pad_hl = int(pad_h / 2.0)
+    pad_hr = pad_h - pad_hl
+
+    return np.pad(image, ((pad_hl, pad_hr), (pad_wl, pad_wr)), 'constant', constant_values = 1.0) 
+    
+def crop_col_chunks(image, col_chunks):
+    img_chunks = []
+    img_h, img_w = image.shape
+    for col_chunk in col_chunks:
+        img_chunks.append(crop_image(image, 0, img_h, *col_chunk))
+    
+    return img_chunks
+
+def find_col_chunks(image, gap_tolerance = 5, chunk_padding = 5):
+    col_means = np.mean(image, axis = 0)
+    nonzero_indices = np.asarray(col_means.nonzero()).flatten()
+    img_h, img_w = image.shape
+
+    chunks = []
+    def add_chunk(start, end):
+        start = start - chunk_padding
+        if start < 0: start = 0
+        
+        end = end + chunk_padding
+        if end > img_w: end = img_w
+
+        chunks.append((start, end)) 
+
+    chunk_start = None
+    prev_idx = 0
+    for idx in nonzero_indices:
+        if chunk_start is None:
+            chunk_start = idx
+        elif (idx - prev_idx) > gap_tolerance:
+            add_chunk(chunk_start, prev_idx)
+            chunk_start = idx
+            
+        prev_idx = idx
+    
+    add_chunk(chunk_start, prev_idx)
+
+    return chunks
+
+def normalize_image_values(image):
+    max_val = max(image.flatten())
+    min_val = min(image.flatten())
+    val_range = max_val - min_val
+    
+    if val_range == 1.0: return image
+    return (image - min_val) / val_range
+
+
+def crop_image(image, row_start, row_end, col_start, col_end):
+    return image[row_start:row_end , col_start:col_end]
+
+def get_crop_bounds(image, padding = 0):
+
+    # compute the means of each row and column to simplify the calc
+    col_means = np.mean(image, axis = 0)
+    row_means = np.mean(image, axis = 1)
+    
+    # find the first and last non-zero values for both row and col
+    def find_start_end(vals):
+        nonzero_indices = np.asarray(vals.nonzero())
+        return min(nonzero_indices.flatten()), max(nonzero_indices.flatten())
+
+    col_start, col_end = find_start_end(col_means)
+    row_start, row_end = find_start_end(row_means)
+    
+    # now add padding
+    img_h, img_w = image.shape
+    col_start = col_start - padding
+    if col_start < 0: col_start = 0
+    
+    col_end = col_end + padding
+    if col_end > img_w: col_end = img_w
+
+    row_start = row_start - padding
+    if row_start < 0: row_start = 0
+    
+    row_end = row_end + padding
+    if row_end > img_h: row_end = img_h
+    
+    return row_start, row_end, col_start, col_end
+
+
+
